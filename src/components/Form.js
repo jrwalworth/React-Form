1842b61bc64e11f3ac932332c4c029fae9e77024
Jason Walworth <jrwalworth@gmail.com>
2022-04-13 16:26:15 -0700

    Added form component and managing state. ToDo- Confirm Password input value.

---
+++ src/components/Form.js
@@ -0,0 +1,78 @@
+import React, {useState} from 'react';
+import './Form.css';
+
+const Form = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = {firstName, lastName, email, password};
+        console.log(newUser);
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPassword('');
+    };
+
+
+    return (
+        <div className="myForm">
+            <form onSubmit={ createUser }>
+                <section className="form-sect">
+                    <label htmlFor="firstName" className="col-25">First Name:</label>
+                    <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) }name="firstName" id="firstName" className="col-75"/>
+                </section>
+                <section className="form-sect">
+                    <label htmlFor="lastName" className="col-25">Last Name:</label>
+                    <input type="text" value={lastName} onChange={ (e) => setLastName(e.target.value) }name="lastName" id="lastName" className="col-75"/>
+                </section>
+                <section className="form-sect">
+                    <label htmlFor="email" className="col-25">Email:</label>
+                    <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) }name="email" id="email" className="col-75"/>
+                </section>
+                <section className="form-sect">
+                    <label htmlFor="password" className="col-25">Password:</label>
+                    <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) }name="password" id="password" className="col-75"/>
+                </section>
+                <section className="form-sect">
+                    <label htmlFor="confirm" className="col-25">Confirm Password:</label>
+                    <input type="password" name="confirm" id="confirm" className="col-75"/>
+                </section>
+                <button type="submit">Create User</button>
+            </form>
+            <div className="form-details">
+                <h2>Your Form Data</h2>
+                <div>
+                    <section className="det-sect">
+                        <h4>First Name:</h4>
+                        <p>{firstName}</p>
+                    </section>
+                    <section className="det-sect">
+                        <h4>Last Name:</h4>
+                        <p>{lastName}</p>
+                    </section>
+                    <section className="det-sect">
+                        <h4>Email:</h4>
+                        <p>{email}</p>
+                    </section>
+                    <section className="det-sect">
+                        <h4>Password:</h4>
+                        <p>{password}</p>
+                    </section>
+                    <section className="det-sect">
+                        <h4>Confirm Password:</h4>
+                        <p>{password}</p>
+                    </section>
+                </div>
+            </div>
+        </div>
+        
+
+    );
+
+}
+
+export default Form;
