7ca67268e8fc858492be1c056869b0f1907edd92
Jason Walworth <jrwalworth@gmail.com>
2022-04-13 19:08:51 -0700

    Updated confirm password state.

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,21 +6,39 @@ const Form = (props) => {
     const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    // const [error, setError] = useState("");
 
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = {firstName, lastName, email, password};
+        const newUser = {firstName, lastName, email, password, confirmPassword};
+        // console.log('Form Submitted:', e.target);
         console.log(newUser);
+        if (email && password) {
+            console.log('Form submitted for', firstName)
+        } else {
+            console.log('Form not submitted.')
+        }
         setFirstName('');
         setLastName('');
         setEmail('');
         setPassword('');
+        setConfirmPassword('');
     };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('form submitted', e.target);
+    }
 
 
     return (
         <div className="myForm">
-            <form onSubmit={ createUser }>
+            {/* {error && error > 10 && <p>foirm not submitted</p>} */}
+            {firstName && firstName < 5 && <p>First name must contain a value.</p>}
+            {lastName && lastName < 5 ? <p>Last name must contain a value.</p> : null}
+            {confirmPassword && password != confirmPassword && <p>Passwords must match.</p>}
+            <form onSubmit={ createUser } >
                 <section className="form-sect">
                     <label htmlFor="firstName" className="col-25">First Name:</label>
                     <input type="text" value={firstName} onChange={ (e) => setFirstName(e.target.value) }name="firstName" id="firstName" className="col-75"/>
@@ -39,7 +57,7 @@ const Form = (props) => {
                 </section>
                 <section className="form-sect">
                     <label htmlFor="confirm" className="col-25">Confirm Password:</label>
-                    <input type="password" name="confirm" id="confirm" className="col-75"/>
+                    <input type="password" value={confirmPassword} onChange={ (e) => setConfirmPassword(e.target.value) }name="confirm" id="confirm" className="col-75"/>
                 </section>
                 <button type="submit">Create User</button>
             </form>
@@ -58,13 +76,14 @@ const Form = (props) => {
                         <h4>Email:</h4>
                         <p>{email}</p>
                     </section>
+                    
                     <section className="det-sect">
-                        <h4>Password:</h4>
+                        <h4>Password:</h4> 
                         <p>{password}</p>
                     </section>
                     <section className="det-sect">
                         <h4>Confirm Password:</h4>
-                        <p>{password}</p>
+                        <p>{confirmPassword}</p>
                     </section>
                 </div>
             </div>
@@ -74,5 +93,7 @@ const Form = (props) => {
     );
 
 }
+//validations?
 
 export default Form;
+
